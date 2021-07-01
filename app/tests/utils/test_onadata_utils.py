4745692ac692ef89bd60193f45a24034b666bc17
Davis Raymond Muro <davisraymondmuro@outlook.com>
2021-07-01 11:04:01 +0300

    Add tests for the `get_csv_export` functionality

--- app/tests/utils/test_onadata_utils.py
+++ app/tests/utils/test_onadata_utils.py
@@ -1,14 +1,16 @@
 """
 Tests for the onadata_utils module
 """
-from unittest.mock import patch
+from pathlib import Path
+from unittest.mock import MagicMock, patch
 
 from httpx._models import Response
 
 from app import schemas
+from app.settings import settings
 from app.models import Server, User, HyperFile
 from app.tests.test_base import TestBase
-from app.utils.onadata_utils import get_access_token
+from app.utils.onadata_utils import get_access_token, get_csv_export
 
 
 class TestOnadataUtils(TestBase):
@@ -45,8 +47,10 @@ class TestOnadataUtils(TestBase):
         assert user.refresh_token != old_refresh_token
         assert User.decrypt_value(user.refresh_token) == "new_token"
 
+    @patch("app.utils.onadata_utils.httpx.get")
+    @patch("app.utils.onadata_utils.get_access_token")
     @patch("app.utils.onadata_utils._get_csv_export")
-    def test_get_csv_export(self, mock_get_csv_export, create_user_and_login):
+    def test_get_csv_export(self, mock_get_csv_export, mock_get_access_token, mock_httpx_get, create_user_and_login):
         """
         Test the get_csv_export function correctly generates the correct
         requests to retrieve a CSV Export
@@ -59,4 +63,29 @@ class TestOnadataUtils(TestBase):
                 user=user.id, filename="test.hyper", is_active=True, form_id="111"
             ),
         )
-        pass
+        file_mock = MagicMock()
+        file_mock.name = '/tmp/test'
+        mock_httpx_get.return_value = Response(
+            json={
+                "id_string": "test"
+            },
+            status_code=200
+        )
+        mock_get_csv_export.return_value = file_mock
+        mock_get_access_token.return_value = "bob"
+
+        ret = get_csv_export(
+            hyperfile, user, user.server, self.db, export_configuration={
+            'include_labels': 'true'
+        })
+        assert ret == Path(file_mock.name)
+        mock_get_access_token.assert_called_with(
+            user, server, self.db
+        )
+        mock_get_csv_export.assert_called_with(
+            f"{server.url}/api/v1/forms/{hyperfile.form_id}/export_async.json?format=csv&include_labels=true",
+            {
+                "user-agent": f"{settings.app_name}/{settings.app_version}",
+                "Authorization": "Bearer bob"
+            }
+        )
