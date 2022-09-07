c9f00c39fb5e2a9e16ecebb1e96acc626675f1d8
Davis Raymond Muro <davisraymondmuro@outlook.com>
2022-09-07 11:48:53 +0300

    Stop usage of deprecated `autocommit` arguement

--- app/database.py
+++ app/database.py
@@ -6,6 +6,6 @@ from app.settings import settings
 
 
 engine = create_engine(settings.database_url, connect_args=settings.db_connect_args)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 Base = declarative_base()
