6540e42a9ec418992e5b09d979df2aa9a8a5db1f
Davis Raymond Muro <davis.ray.muro@gmail.com>
2023-06-08 18:45:47 +0300

    refactor(crud-user): Add helper functions to help with upstream syncing

--- app/crud/crud_hyperfile.py
+++ app/crud/crud_hyperfile.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from typing import List
+import sentry_sdk
 
 from sqlalchemy.orm import Session
 from app.core.onadata import FailedExternalRequest, OnaDataAPIClient
@@ -8,16 +9,17 @@ from app.core.security import fernet_decrypt
 from app.crud.base import CRUDBase
 from app.core.config import settings
 from app.libs.s3.client import S3Client
+from app.libs.tableau.client import InvalidConfiguration, TableauClient
 from app.models.hyperfile import HyperFile
 from app.models.user import User
-from app.schemas.hyperfile import FileCreate, FilePatchRequestBody
+from app.schemas.hyperfile import FileCreate, FilePatchRequestBody, FileStatusEnum
 from app.utils.onadata_utils import UnsupportedForm
 
 
 class CRUDHyperFile(CRUDBase[HyperFile, FileCreate, FilePatchRequestBody]):
     def create(self, db: Session, *, obj_in: FileCreate, user: User) -> HyperFile:
         client = OnaDataAPIClient(
-            user.server.url, fernet_decrypt(user.token.token_value), user
+            user.server.url, fernet_decrypt(user.access_token), user
         )
         try:
             form_data = client.get_form(obj_in.form_id)
@@ -40,6 +42,43 @@ class CRUDHyperFile(CRUDBase[HyperFile, FileCreate, FilePatchRequestBody]):
             .filter(self.model.form_id == form_id, self.model.user_id == user_id)
             .all()
         )
+
+    def get_local_path(self, *, obj: HyperFile) -> str:
+        return f"{settings.MEDIA_ROOT}/{obj.form_id}_{obj.filename}"
+
+    def get_latest_file(self, *, obj: HyperFile) -> str:
+        local_path = self.get_local_path(obj=obj)
+        s3_client = S3Client()
+        s3_client.download(self.get_file_path(obj=obj), local_path)
+        return local_path
+
+    def update_status(
+        self, db: Session, *, obj: HyperFile, status: FileStatusEnum
+    ) -> HyperFile:
+        obj.file_status = status
+        db.add(obj)
+        db.commit()
+        db.refresh(obj)
+        return obj
+
+    def sync_upstreams(self, db: Session, *, obj: HyperFile):
+        s3_client = S3Client()
+        s3_client.upload(self.get_local_path(obj=obj), self.get_file_path(obj=obj))
+
+        if obj.configuration:
+            tableau_client = TableauClient(configuration=obj.configuration)
+            try:
+                tableau_client.validate_configuration(obj.configuration)
+            except InvalidConfiguration as e:
+                sentry_sdk.capture_exception(e)
+                pass
+            else:
+                tableau_client.publish_hyper(self.get_file_path(obj=obj))
+        obj.last_updated = datetime.utcnow()
+        db.add(obj)
+        db.commit()
+        db.refresh(obj)
+        return obj
 
     def get_file_path(self, *, obj: HyperFile) -> str:
         return f"{obj.user.server_id}/{obj.user.username}/{obj.form_id}_{obj.filename}"
