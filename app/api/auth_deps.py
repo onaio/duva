f339f242b50ee45b380c30eddceb1e0781557ae9
Ukang'a Dickson <ukanga@users.noreply.github.com>
2024-11-19 12:17:23 +0300

    Additional user logging for debugging purposes

--- app/api/auth_deps.py
+++ app/api/auth_deps.py
@@ -1,3 +1,4 @@
+import logging
 import jwt
 from fastapi import Depends, HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer
@@ -11,6 +12,8 @@ from app.schemas.token import TokenPayload
 
 reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="/api/v1/oauth/login", auto_error=False)
 
+logger = logging.getLogger("auth_deps")
+
 
 def get_current_user(
     db: Session = Depends(get_db),
@@ -18,8 +21,10 @@ def get_current_user(
     *,
     request: Request,
 ) -> User:
+    logger.info(f"Get current user token {token}")
     if not token:
         token = request.session.get("token")
+        logger.info(f"Get current user session token {token}")
 
     if token:
         try:
@@ -28,6 +33,7 @@ def get_current_user(
             )
             token_data = TokenPayload(**payload)
         except jwt.PyJWTError:
+            logger.error(f"Get current user could not validate credentials {token}")
             if request.session.get("token"):
                 del request.session["token"]
             raise HTTPException(
@@ -36,8 +42,10 @@ def get_current_user(
 
         user = crud.user.get(db, id=token_data.sub)
         if not user:
+            logger.info(f"Get current user: user not found {token_data.sub}")
             raise HTTPException(status_code=401, detail="User not found")
 
         return user
-    else:
-        raise HTTPException(status_code=401, detail="Could not validate credentials")
+    logger.info(f"Get current user: missing token {token}")
+
+    raise HTTPException(status_code=401, detail="Could not validate credentials")
