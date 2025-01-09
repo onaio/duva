fa926979f002fbb3a2a43361ffa96df86adaa2f5
Ukang'a Dickson <ukanga@users.noreply.github.com>
2025-01-09 14:52:52 +0300

    chore: ensure MEDIA_ROOT DIR is created on rq worker startup

--- app/jobs/settings.py
+++ app/jobs/settings.py
@@ -14,5 +14,8 @@ sentry_dsn = str(settings.SENTRY_DSN) if settings.SENTRY_DSN else None
 if sentry_dsn:
     sentry_sdk.init(sentry_dsn, integrations=[RqIntegration()])
 
+if not os.path.isdir(settings.MEDIA_ROOT):
+    os.mkdir(settings.MEDIA_ROOT)
+
 REDIS_URL = settings.REDIS_URL
 QUEUES = [os.environ.get("QUEUE_NAME", "default")]
